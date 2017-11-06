e82566a321cefe7f15088390107d02cde9ece94d
DanielHan <2420626787@qq.com>
2017-11-06 18:53:20 +0800

    还差饼状图

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,22 +2,30 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+    private Paint mPaint;
 
     public Practice2DrawCircleView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     @Override
@@ -26,5 +34,21 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        mPaint.setColor(Color.BLACK);
+        canvas.drawCircle(450, 200, 200, mPaint);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+        canvas.drawCircle(1000, 200, 200, mPaint);
+
+        mPaint.setColor(0xff4A90E2);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(450, 700, 200, mPaint);
+
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(80);
+        canvas.drawCircle(1000, 700, 200, mPaint);
+
     }
 }
